56abd9ae5e588301e9e66eedfab97fcd75fa6b83
Bhushan Sah <shahbhushan73@gmail.com>
2024-08-31 02:52:42 +0530

    new line added

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
-int main (){
+#include <stdio.h>
+int main()
+{
     printf("Hello this is before commit");
-    
+    printf("AUur bhai logg  ");
+    printf("AUur bhai sab thikk  ");
+
 }
