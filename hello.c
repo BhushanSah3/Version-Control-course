3ab6ff1b98f5b183600136e6b1bd7c129a902044
Bhushan Sah <shahbhushan73@gmail.com>
2024-08-31 21:13:07 +0530

    Second file

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello this is before commit");
+    printf("Hello this is before commit"); 
     printf("AUur bhai logg  ");
     printf("AUur bhai sab thikk  ");
 
